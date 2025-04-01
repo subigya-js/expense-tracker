5e2c6fd961d4438c76f5b98d6944784f833b819c
subigya-js <developer.subigya@gmail.com>
2025-04-01 08:34:12 +0530

    style: enhance ExpenseBreakdown component styling; make category labels bold and adjust percentage text size for better readability

--- frontend/src/app/components/overview/ExpenseBreakdown.tsx
+++ frontend/src/app/components/overview/ExpenseBreakdown.tsx
@@ -77,12 +77,12 @@ const ExpenseBreakdown = () => {
         <div className="space-y-4 border h-[300px] sm:h-[420px] rounded-md py-10 px-6 shadow">
             {Object.entries(categoryBreakdown).map(([category, { total, percentage }]) => (
                 <div key={category} className="space-y-2">
-                    <div className="flex justify-between">
+                    <div className="flex justify-between font-semibold">
                         <span>{category}</span>
                         <span>${total.toFixed(2)}</span>
                     </div>
                     <Progress value={percentage} className="w-full" />
-                    <span>{percentage.toFixed(2)}% of total expenses</span>
+                    <span className='text-sm'>{percentage.toFixed(2)}% of total expenses</span>
                 </div>
             ))}
         </div>
